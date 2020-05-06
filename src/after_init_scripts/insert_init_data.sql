c0326548044a2165042f880376e9575d6c18dfef
castic96 <zdenek.castoral.jr@gmail.com>
2020-05-06 20:38:30 +0200

    - založení souborů pro skripty - vytvořen skript pro naplnění tabulek inicializačními daty - insert_init_data.sql

---
+++ src/after_init_scripts/insert_init_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO stav (id, nazev) VALUES (1, 'rozehraná');
+INSERT INTO stav (id, nazev) VALUES (2, 'vítězství začínajícího hráče');
+INSERT INTO stav (id, nazev) VALUES (3, 'prohra začínajícího hráče');
+INSERT INTO stav (id, nazev) VALUES (4, 'remíza');
+
+COMMIT;
+
+
+INSERT INTO omezeni (radek_max, radek_min, sloupec_max, sloupec_min, symbol_max, symbol_min) 
+VALUES (20, 5, 20, 5, 15, 5);
+
+COMMIT;
